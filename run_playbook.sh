7b3429bfb309425ebecb281c0ad3caf313237bca
Ulf Nilsson <unilsson@ipfabriken.se>
2025-05-19 18:34:04 +0200

    First upload

---
+++ run_playbook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if a playbook file was provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <playbook.yml>"
+  exit 1
+fi
+
+PLAYBOOK="$1"
+HOSTS="$2"
+
+# Check if the file exists
+if [ ! -f "$PLAYBOOK" ]; then
+  echo "Error: Playbook '$PLAYBOOK' not found."
+  exit 2
+fi
+
+# Run the Ansible playbook
+ansible-playbook -i ./inventory/inventory.yaml "$PLAYBOOK" -l "$HOSTS" --extra-vars "@vault.yaml" --vault-password-file ~/.vault_pass.txt
